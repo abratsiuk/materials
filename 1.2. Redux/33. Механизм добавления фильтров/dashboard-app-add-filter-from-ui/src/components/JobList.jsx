3b95599ac25ec3989d9149ca0e8772abde3e69d4
Aleksandr Bratsiuk <bratsiuk@outlook.com>
2025-01-29 13:49:50 +0200

    ...

---
+++ 1.2. Redux/33. Механизм добавления фильтров/dashboard-app-add-filter-from-ui/src/components/JobList.jsx
@@ -0,0 +1,28 @@
+import {useSelector, useDispatch} from 'react-redux';
+import { JobPosition } from './JobPosition';
+import {selectAllPositions} from 'store/positions/position-selectors';
+
+import {addFilter} from 'store/filters/filter-actions';
+
+const JobList = () => {
+  const dispatch = useDispatch();
+  const positions = useSelector(selectAllPositions);
+
+  const handleAddFilter = (filter) => {
+    dispatch(addFilter(filter));
+  };
+
+  return (
+    <div className='job-list'>
+      {positions.map(item => (
+        <JobPosition
+          key={item.id}
+          handleAddFilter={handleAddFilter}
+          {...item}
+        />
+      ))}
+    </div>
+  )
+}
+
+export {JobList};
